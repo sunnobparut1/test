fc69c178b066fa960600b478117cc81f6ea1d645
Nobparut <nobparut.p@rmutsvmail.com>
2024-10-03 04:25:32 +0000

    add a.c

--- a.c
+++ a.c
@@ -3,3 +3,6 @@ int main(){
 	print("hello git\n");
 	return 0;
 }
+int sub(int a, int b){
+	return a+b;
+}
--- add.c
+++ add.c
@@ -1,3 +1,8 @@
+#include<studio.h>
+int mail(){
+	printf("hello git\n");
+	return 0;
+}
 int add(int a, int b){
 	return a+b;
 }
